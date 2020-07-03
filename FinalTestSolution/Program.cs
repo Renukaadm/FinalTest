05519ed353c56e02b2e8e4a7e5cb445ccea969cf
Renuka <renuka.aedma@gmail.com>
2020-07-02 20:35:17 -0400

    Changed for test

--- FinalTestSolution/Program.cs
+++ FinalTestSolution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace FinalTestSolution
 {
@@ -6,7 +7,49 @@ namespace FinalTestSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Object[] ObjectsArray = new Object[2];
+            ObjectsArray[0] = new Adult("parent1", "parent1");
+            ObjectsArray[1] = new Child("Child1","Child1");
+            
+            foreach (var item in ObjectsArray)
+            {
+                System.Console.WriteLine(item);
+                
+            }
+
+
         }
     }
+
+    public class Adult
+    {
+        string FirstName;
+        string LastName;
+        public Adult(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        public override string ToString()
+        {
+            return this.FirstName+" " + this.LastName;
+        }
+    }
+
+    public class Child
+    {
+        string FirstName;
+        string LastName;
+       public Child(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+   
 }
